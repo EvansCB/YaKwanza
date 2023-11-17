ce132f5bf6900f0764bd9ea9ed6be04f0a7ae6bb
Evans Bet <evansworkit@outlook.com>
2023-11-17 15:37:54 +0300

    Add Admin

--- myprojects/admin.py
+++ myprojects/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from myprojects.models import MyProject
+
+class MyProjectAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(MyProject,MyProjectAdmin)
--- myprojects/models.py
+++ myprojects/models.py
@@ -8,3 +8,4 @@ class MyProject(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     technology = models.CharField(max_length=20)
+    image = models.FileField(upload_to="myproject_images/", blank=True)
